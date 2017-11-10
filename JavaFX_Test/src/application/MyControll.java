509322272475a777d1415cf8a70ac1aa19f8d71d
allen.wang <allen.wang@tooling-intelligence.com>
2017-11-10 13:59:59 +0800

    some test
    1. Textarea
    2. Radio Button

--- JavaFX_Test/src/application/MyControll.java
+++ JavaFX_Test/src/application/MyControll.java
@@ -6,11 +6,17 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class MyControll implements Initializable {
 	@FXML
@@ -18,11 +24,37 @@ public class MyControll implements Initializable {
 
 	@FXML
 	private TextField myTextField;
-
+	@FXML
+	private TextArea logBox;
+	@FXML
+	private ComboBox<String> comboBox;
+	@FXML
+	private RadioButton radio1;
+	@FXML
+	private RadioButton radio2;
+	//单选框
+	private ToggleGroup group;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
 		// TODO (don't really need to do anything here).
+		
+		logBox.setMaxHeight(Double.MAX_VALUE);
+		logBox.setMaxWidth(Double.MAX_VALUE);
+		logBox.setEditable(false);
+		logBox.setWrapText(true);
+		
+		ObservableList<String> options = FXCollections.observableArrayList("Option 1", "Option 2", "Option 3");
+		comboBox.setItems(options);
+		
+		
+		//单选框
+		group = new ToggleGroup();
+		radio1.setToggleGroup(group);
+		radio2.setToggleGroup(group);
+		radio1.setUserData("1");
+		radio2.setUserData("2");
 
 	}
 
@@ -37,6 +69,17 @@ public class MyControll implements Initializable {
 		String dateTimeString = df.format(now);
 		// Show in VIEW
 		myTextField.setText(dateTimeString);
+		
+		logBox.setText(logBox.getText()+ dateTimeString + "\n");
+		
+		System.out.println(group.getSelectedToggle().getUserData().toString());
 
 	}
+	
+	public void selectOption(){
+		System.out.println(comboBox.getSelectionModel().getSelectedIndex());
+		System.out.println(comboBox.getSelectionModel().getSelectedItem());
+		System.out.println(comboBox.getValue());
+	}
+	
 }
--- TestDO/src/test.java
+++ TestDO/src/test.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import com.tools.GetDateDistance;
 import com.tools.SimpleDate;
 
@@ -7,7 +11,7 @@ public class test {
 		//获得程序当前目录
 		//System.out.println(System.getProperty("user.dir"));
 		
-		SimpleDate sd = new SimpleDate();
+/*		SimpleDate sd = new SimpleDate();
 		
 		//获取天数间隔
 		int days = GetDateDistance.getDaysSpace("1600-01-01", sd.getSimpleDateYMD());
@@ -15,6 +19,26 @@ public class test {
 		
 		System.out.println(sd.getSimpleDateYMD());
 		
+		*/
+		
+		
+		List l = new ArrayList();
+		l.add(1);
+		l.add(11);
+		l.add(111);
+		l.add(1111);
+		l.add(11111);
+		
+		Iterator it = l.iterator();
+		while(it.hasNext()){
+			Integer intV = (Integer)it.next();
+			if(intV==111){
+				it.remove();
+			}
+			
+		}
+		
 	}
 
+	
 }
